d1c43070b41cbc2e023a69422c77de15de660c60
BettyYujun <31697069+BettyYujun@users.noreply.github.com>
2018-09-29 11:33:10 +0800

    第二次提交

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("this is my first line");
+            Console.WriteLine("this is my second line");
         }
     }
 }
